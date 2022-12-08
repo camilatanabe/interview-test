c06b096e69145b04a7eaa291350ce77d85f3bfdd
unknown <camila_tanabe@hotmail.com>
2022-12-08 23:46:32 +0900

    feat: adding card container and cards components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './style.scss'
 
-const Button = ({ outlined, desktopOnly, children, onClick }) => {
+const Button = ({
+  outlined,
+  desktopOnly,
+  classNameProps,
+  children,
+  onClick
+}) => {
   return (
     <button
       className={`button ${outlined ? 'button__outlined' : ''} ${
         desktopOnly ? 'button__desktopOnly' : ''
-      }`}
+      } ${classNameProps}`}
       onClick={onClick}
       type="button"
     >
